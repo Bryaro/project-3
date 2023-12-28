d2566a30c224d4ecd0af7ffdd996c92ba5f03296
Bryar Osman <bryarosman.bo@gmail.com>
2023-12-28 18:25:37 +0000

    Add player_turn function and docstring to it

--- run.py
+++ run.py
@@ -2,10 +2,12 @@ import random
 
 def create_board():
     """
-    function for creating Tic-Tac_Toe table/board
-    Return a 3x3 Tic-Tac-Toe board numbered from 1 to 9.
+    Create and return a 3x3 Tic-Tac-Toe board numbered from 1 to 9.
     Each cell of the grid is filled with a number, starting from 1 in the
     top-left cell and ending with 9 in the bottom-right cell.
+    Returns:
+    list of lists: A 3x3 grid representing the Tic-Tac-Toe board,
+    with each cell numbered from 1 to 9.
     """
     board = []
     cell_number = 1
@@ -16,7 +18,7 @@ def create_board():
             cell_number += 1
         board.append(row)
     return board
-print("Table/board for Tic-Tac-Toe")
+print("\nTable/board for Tic-Tac-Toe\n")
 
 
 def add_list_on_list():
@@ -29,5 +31,16 @@ def add_list_on_list():
     for row in board:
         print(row)
 
-list_block = add_list_on_list()
-print(list_block)
+add_list_on_list()
+
+
+def player_turn():
+    """
+    Create player turn
+    lets player type number from 1-9
+    raise error if player chose incorrect or already chosen data
+    this will run in while loop till player/user chosen correct data
+    """
+    print("\nPlayers turn, chose nr fr 1-9\n")
+    
+player_turn()
